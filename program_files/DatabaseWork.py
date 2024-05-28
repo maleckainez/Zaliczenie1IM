5cde99db1351c5f6d6bff537c7fe67b9c4c1b040
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-28 22:08:13 +0200

    Files rebase

--- program_files/DatabaseWork.py
+++ program_files/DatabaseWork.py
@@ -1,19 +1,40 @@
-'''class PoczątkowaBazaDanych():
-    with sqlite3.connect("program_files/databases/baza_danych_userow.db") as connection:
-        cursor = connection.cursor()
+import sqlite3
+import hashlib
+with sqlite3.connect("../program_files/databases/testowe.db") as connection:
+    cursor = connection.cursor()
 
-        create_table_query = """
+    create_table_query = """
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 login TEXT UNIQUE NOT NULL,
-                imie TEXT NOT NULL,
+                imie TEXT  NOT NULL,
                 nazwisko TEXT NOT NULL,
                 haslo_hash TEXT NOT NULL,
-                uprawnienia TEXT NOT NULL
-            );
+                uprawnienia TEXT NOT NULL );
             """
-        haslo_hash_poczatek = "admin"
-        haslo_hash_poczatek = hashlib.sha256(haslo_hash_poczatek.encode()).hexdigest()
-        cursor.execute(create_table_query)
+    cursor.execute(create_table_query)
+    login="admin"
+    imie="admin"
+    nazwisko="admin"
+    haslo="admin"
+    haslo_hash = hashlib.sha256(haslo.encode()).hexdigest()
+    uprawnienia ="admin"
+    add_user_query = "INSERT INTO users (login, imie, nazwisko, haslo_hash, uprawnienia) VALUES (?, ?, ?, ?, ?);"
+    cursor.execute(add_user_query,(login, imie, nazwisko, haslo_hash, uprawnienia))
 
-        connection.commit() '''
+
+    create_table_query = """
+        CREATE TABLE IF NOT EXISTS rejestr (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            rodzaj_dokumentu TEXT NOT NULL,
+            nr_wewnetrzny TEXT UNIQUE NOT NULL,
+            nadawca_dokumentu TEXT NOT NULL,
+            odbiorca_dokumentu TEXT NOT NULL,
+            tytul_pisma TEXT NOT NULL,
+            krotki_opis TEXT,
+            kod_katalogowy TEXT UNIQUE NOT NULL );
+        """
+    cursor.execute(create_table_query)
+
+    connection.commit()
+
