85a64d54cc8fc31003c3dee12e23d516cee2b4cc
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-30 21:20:47 +0200

    Puthon ui modified

--- program_files/databases/DatabaseWork.py
+++ program_files/databases/DatabaseWork.py
@@ -5,26 +5,26 @@ with sqlite3.connect("/Users/inezmalecka/Desktop/Studiowanie/Zaliczenie1IM/progr
     drop_table = "DROP TABLE IF EXISTS rejestr "
     cursor.execute(drop_table)
     connection.commit()
-#
-#
-#    create_table_query = """
-#            CREATE TABLE IF NOT EXISTS users (
-#                id INTEGER PRIMARY KEY AUTOINCREMENT,
-#                login TEXT UNIQUE NOT NULL,
-#                imie TEXT  NOT NULL,
-#                nazwisko TEXT NOT NULL,
-#                haslo_hash TEXT NOT NULL,
-#                uprawnienia TEXT NOT NULL );
-#            """
-#    cursor.execute(create_table_query)
-#    login="admin"
-#   imie="admin"
-#    nazwisko="admin"
-#    haslo="admin"
-#    haslo_hash = hashlib.sha256(haslo.encode()).hexdigest()
-#    uprawnienia ="admin"
-#    add_user_query = "INSERT INTO users (login, imie, nazwisko, haslo_hash, uprawnienia) VALUES (?, ?, ?, ?, ?);"
-#    cursor.execute(add_user_query,(login, imie, nazwisko, haslo_hash, uprawnienia))
+
+
+    create_table_query = """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                login TEXT UNIQUE NOT NULL,
+                imie TEXT  NOT NULL,
+                nazwisko TEXT NOT NULL,
+                haslo_hash TEXT NOT NULL,
+                uprawnienia TEXT NOT NULL );
+            """
+    cursor.execute(create_table_query)
+    login="admin"
+    imie="admin"
+    nazwisko="admin"
+    haslo="admin"
+    haslo_hash = hashlib.sha256(haslo.encode()).hexdigest()
+    uprawnienia ="admin"
+    add_user_query = "INSERT INTO users (login, imie, nazwisko, haslo_hash, uprawnienia) VALUES (?, ?, ?, ?, ?);"
+    cursor.execute(add_user_query,(login, imie, nazwisko, haslo_hash, uprawnienia))
 
 
     create_table_query = """
